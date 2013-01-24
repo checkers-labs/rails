978310e49b28fe3ed3fabf2b4bf5303af50c8288
etienne-dd <106606@supinfo.com>
2013-01-23 18:36:07 -0800

    model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
-      attr_accessible :password, :username, :email, :password_confirmation
+      attr_accessible :username, :email, :password, :password_confirmation
       attr_accessor :password
 
-      validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
-      validates :email, :presence => true, :uniqueness => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
-      validates :password, :confirmation => true
-      validates_length_of :password, :in => 6..20, :on => :create
+      validates :username, :presence => true, 
+                           :uniqueness => true, 
+                           :length => { :within => 3..20 }
+      validates :email, :presence => true, 
+                        :uniqueness => true, 
+                        :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
+      validates :password, :confirmation => true, 
+                           :length       => { :within => 6..20 }
 end
