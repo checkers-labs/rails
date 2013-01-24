fbfbc4661af737faf3e65e3cae1c486df085b262
momohuri <maurin.lenglart@gmail.com>
2013-01-23 18:56:54 -0800

    encrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
+require 'bcrypt'
+
+
 class User < ActiveRecord::Base
+    
+      before_save :hash_password
+  
+  
       attr_accessible :username, :email, :password, :password_confirmation
-      attr_accessor :password
+      attr_accessor :password_confirmation, :password
 
       validates :username, :presence => true, 
                            :uniqueness => true, 
@@ -10,4 +17,17 @@ class User < ActiveRecord::Base
                         :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
       validates :password, :confirmation => true, 
                            :length       => { :within => 6..20 }
+                         
+                         
+      private
+      def hash_password
+         self.salt = BCrypt::Engine.generate_salt
+          self.encrypted_password = BCrypt::Engine.hash_secret(password, salt)
+      end  
+                           
+   
+   
+   
+   
+
 end
