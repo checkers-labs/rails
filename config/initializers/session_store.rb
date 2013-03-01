a23a414fec30d30fa2ed6e76ac3c78ff2a9cf477
etienne-dd <106606@supinfo.com>
2013-02-28 19:59:41 -0800

    merge

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-<<<<<<< HEAD
-First::Application.config.session_store :redis_store , key: '_first_session'
-=======
 First::Application.config.session_store :redis_store, :redis_server => { :namespace => 'session' }, key: '_first_session'
->>>>>>> fd29aaa99e98f35862f76ae3b865a8577d62acb2
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
