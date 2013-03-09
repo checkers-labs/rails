fe092f0dbba9099d9edb85f3fb3c7087491cb1a2
momohuri <maurin.lenglart@gmail.com>
2013-03-08 23:10:24 -0800

    game in redis

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,12 @@
 class GameController < ApplicationController
-  def acceptInvite    
+  
+  def acceptInvite     
+   key = $redis.keys("invitation:from#{params[:id]}to*")
+   $redis.del(key)   
+    
+    game = "game:from#{params[:id]}to#{session[:user_id]}"   
+    userJson = [session[:user_id], params[:id]].to_json 
+    $redis.set(game,userJson)
     render :text => true, :content_type => "text/plain"
   end
   
