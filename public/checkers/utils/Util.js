3b1a49d68f0e4d29b6ff2a5c4cdde202233ab4f2
momohuri <maurin.lenglart@gmail.com>
2013-04-27 16:58:27 -0700

    finish game 2

--- public/checkers/utils/Util.js
+++ public/checkers/utils/Util.js
@@ -1,93 +1,101 @@
 define(['libraries/oXHR', 'config/constants'], function(oXHR, c) {
-        return {            
-            getMapJSON: function(name) {
-                var xhr = oXHR.getXMLHttpRequest();
-                xhr.open("GET", '/checkers/config/maps/' + name + '.json', false);
-                xhr.send(null);
-                if(xhr.readyState != 4 || (xhr.status != 200 && xhr.status != 0)) {
-                    throw new Error("Impossible de charger la carte nommée \"" + name + "\" (code HTTP : " + xhr.status + ").");
+    return {
+        getMapJSON : function(name) {
+            var xhr = oXHR.getXMLHttpRequest();
+            xhr.open("GET", '/checkers/config/maps/' + name + '.json', false);
+            xhr.send(null);
+            if (xhr.readyState != 4 || (xhr.status != 200 && xhr.status != 0)) {
+                throw new Error("Impossible de charger la carte nommée \"" + name + "\" (code HTTP : " + xhr.status + ").");
+            }
+            return xhr.responseText;
+        },
+        coordinateToPos : function(x, y) {
+            var coordinate = new Array(x / c.WIDTH_TILE, y / c.HEIGHT_TILE);
+            return coordinate;
+        },
+        posToCoordinate : function(x, y) {
+            var coordinate = new Array(x * c.WIDTH_TILE, y * c.HEIGHT_TILE);
+            return coordinate;
+        },
+        addAlert : function(text, type) {
+            switch(type) {
+                case 'info':
+                    $('#game_alert-info').html(text);
+                    break;
+                case 'error':
+                    $('.alert-error').show();
+                    $('#game_alert-error').html(text);
+                    break;
+            }
+        },
+        supprAlert : function() {
+            $('.alert-error').hide();
+        },
+        sendMove : function(posBefore, posAfter, again) {
+            var self = this;
+            $.ajax({
+                type : "POST",
+                url : "/setMove",
+                dataType : "json",
+                data : {
+                    pawnBefore : posBefore,
+                    pawnAfter : posAfter,
+                    again : again
+                },
+                success : function(data, textStatus, jqXHR) {
+                    // si c'est à l'autre de jouer
+                    if (!again) {
+                        window.maxTurn--;
+                        if (window.Map.isPlayerDead() != 2) {
+                            alert('finish');
+                        }
+                        if (window.maxTurn == 0) {
+                            alert('finish');
+                        }
+                        window.turn = window.turn == 1 ? 0 : 1;
+                        self.addAlert("C'est à votre adversaire de jouer !", "info");
+                        self.getMove();
+                    }
                 }
-                return xhr.responseText;
-            },
-            coordinateToPos: function(x, y) {
-                var coordinate = new Array(x/c.WIDTH_TILE, y/c.HEIGHT_TILE);
-                return coordinate;
-            },
-            posToCoordinate: function(x, y) {
-                var coordinate = new Array(x*c.WIDTH_TILE, y*c.HEIGHT_TILE);
-                return coordinate;
-            },
-            addAlert: function(text, type) {
-                switch(type) {
-                    case 'info':
-                        $('#game_alert-info').html(text);
-                        break;
-                    case 'error':
-                        $('.alert-error').show();
-                        $('#game_alert-error').html(text);
-                        break;
-                }
-            },
-            supprAlert: function(){                
-                $('.alert-error').hide();
-            },
-            sendMove: function(posBefore,posAfter,again) {
-                var self=this;
-                $.ajax({
-                    type: "POST",
-                    url: "/setMove",
-                    dataType: "json",
-                    data: { pawnBefore: posBefore,
-                        pawnAfter: posAfter,
-                        again:again
-                    },
-                    success:function(data, textStatus, jqXHR){
-                        // si c'est à l'autre de jouer
-                        if(!again) {
+            });
+        },
+        getMove : function() {
+            var self = this;
+            $.ajax({
+                type : "GET",
+                url : "/getMove",
+                dataType : "json",
+                success : function(data, textStatus, jqXHR) {
+                    if (data) {
+                        for (var i = 1; i < data.length; i += 2) {
+                            var pawn = window.Map.grid[data[i][1]][data[i][0]];
+                            var posPawn = {
+                                x : data[i+1][0],
+                                y : data[i+1][1]
+                            };
+                            pawn.move(posPawn);
+                        }
+                        // si à nous de jouer
+                        if (!JSON.parse(data[0])) {
                             window.maxTurn--;
-                            if(window.maxTurn==0){
+                            if (window.Map.isPlayerDead() != 2) {
+                                alert('finish');
+                            }
+                            if (window.maxTurn == 0) {
                                 alert('finish');
                             }
                             window.turn = window.turn == 1 ? 0 : 1;
-                            self.addAlert("C'est à votre adversaire de jouer !", "info");
+                            self.addAlert('A votre tour de jouer ;)', 'info');
+                        } else {
                             self.getMove();
-                        }                                   
-                    }                          
-                });  
-            },
-            getMove: function() {
-                var self=this;
-                $.ajax({
-                    type: "GET",
-                    url: "/getMove",
-                    dataType: "json",
-                    success:function(data, textStatus, jqXHR){                        
-                        if(data){                            
-                            for(var i=1 ; i<data.length ; i+=2){
-                                var pawn = window.Map.grid[data[i][1]][data[i][0]];
-                                var posPawn = {x: data[i+1][0], y:data[i+1][1]};
-                                pawn.move(posPawn);
-                            }
-                            // si à nous de jouer
-                            if(!JSON.parse(data[0])) {
-                                 window.maxTurn--;
-                                 if(window.Map.getNumberPawns()!=2){
-                                     alert('finish');
-                                 }
-                                if(window.maxTurn==0){
-                                    alert('finish');
-                                }
-                                window.turn = window.turn == 1 ? 0 : 1;
-                                self.addAlert('A votre tour de jouer ;)', 'info');
-                            } else {
-                                self.getMove();
-                            }
-                        }else{                                
-                           setTimeout(function () { self.getMove(); }, 2000); 
                         }
-                    }     
-                });
-            }
-        };
-    }
-);
+                    } else {
+                        setTimeout(function() {
+                            self.getMove();
+                        }, 2000);
+                    }
+                }
+            });
+        }
+    };
+});
