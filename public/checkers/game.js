f3d2f90005b8360d7dbce18c6444c23dac94efa2
etienne-dd <106606@supinfo.com>
2013-05-03 18:23:13 -0700

    ajout du loading en canvas au chargement de la game

--- public/checkers/game.js
+++ public/checkers/game.js
@@ -4,9 +4,33 @@ requirejs.config({
 });
 
 // Start the main app logic.
-requirejs(['libraries/kineticjs-v433/kinetic', 'libraries/underscore', 'config/constants', 'utils/Resource', 'gui/Map', 'utils/Util'],
-function (kinetic, underscore, c, Resource, Map, Util) {
+requirejs(['libraries/kineticjs-v433/kinetic', 'libraries/underscore', 'libraries/sonic', 'config/constants', 'utils/Resource', 'gui/Map', 'utils/Util'],
+function (kinetic, underscore, sonic, c, Resource, Map, Util) {
+    var square = new Sonic({
+        width: 100,
+        height: 50,
+        stepsPerFrame: 1,
+        trailLength: 1,
+        pointDistance: .1,
+        fps: 15,
+        padding: 10,
+        //step: 'fader',
+
+        fillColor: '#FF2E82',
+
+        setup: function() {
+            this._.lineWidth = 20;
+        },
+        path: [
+            ['line', 0, 20, 100, 20],
+            ['line', 100, 20, 0, 20]
+        ]
+    });
+    square.play();
+    $('#loading').append(square.canvas);
     Resource.init(function() {
+        square.stop();
+        $('#loading').hide();
         // on set le tour pour le joueur et on passe la Map en global
         window.player = document.cookie.split('=')[1];
         window.turn = 0;
